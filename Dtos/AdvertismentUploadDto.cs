b36ed567061435a005f5503e5852caf83069610e
Thanveerahmd <ahamedthanveer123@gmail.com>
2019-07-17 18:31:23 +0530

    gfh

--- Dtos/AdvertismentUploadDto.cs
+++ Dtos/AdvertismentUploadDto.cs
@@ -5,12 +5,13 @@ namespace pro.backend.Dtos
 {
     public class AdvertismentUploadDto
     {
-        public int MyProperty { get; set; }
+        public int id { get; set; }
         public string Description { get; set; }
         public DateTime DateAdded { get; set; }
         public string PublicID { get; set; }
         public int ProductId { get; set; }
         public string PaymentStatus { get; set; }
         public string Status { get; set; }
+        public int timestamp { get; set; }
     }
 }
