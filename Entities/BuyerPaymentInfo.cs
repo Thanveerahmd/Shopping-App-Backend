3e70abd12d7327f8bb88969470a4ac53ea990bf1
Thanveerahmd <ahamedthanveer123@gmail.com>
2019-07-18 22:15:07 +0530

    UpdatePaymentInfo

--- Entities/BuyerPaymentInfo.cs
+++ Entities/BuyerPaymentInfo.cs
@@ -7,14 +7,14 @@ namespace pro.backend.Entities
     {
         public int Id { get; set; }
         public int order_id { get; set; }
-        public int payment_id { get; set; }
+        public long payment_id { get; set; }
         public float payhere_amount { get; set; }
         public string payhere_currency { get; set; }
         public int status_code { get; set; }
         public string method { get; set; }
         public string status_message { get; set; }
         public string card_holder_name { get; set; }
-        public int card_no { get; set; }
+        public string card_no { get; set; }
         public string card_expiry { get; set; }
         public string UserId { get; set; }
         public DateTime DateOfPayment { get; set; }
