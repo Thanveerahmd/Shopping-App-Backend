e391bd200d4fdc2cde94bc944d8a8a941bb08fb5
Thanveerahmd <ahamedthanveer123@gmail.com>
2019-07-30 10:51:12 +0530

    space

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
